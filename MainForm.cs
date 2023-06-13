ce8f007c5add427d00518c934914a8c02020b6a1
Harshal Abhiman Thakare <99874760+HarshalThakare8055@users.noreply.github.com>
2023-06-13 09:58:54 +0530

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotePad__
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void notepadUltraProMaxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is my Notepad Ultra Pro Max Application","Notepad Ultra Pro Max About as",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void nToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|Rich Text Files(*.rtf)|*.rtf";
+            
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if (Path.GetExtension(openFileDialog.FileName)==".txt")
+                {
+                    MainRichTextBox.LoadFile(openFileDialog.FileName,RichTextBoxStreamType.PlainText);
+                }
+                if (Path.GetExtension(openFileDialog.FileName) == ".rtf")
+                {
+                    MainRichTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.RichText);
+                }
+                this.Text = Path.GetFileName(openFileDialog.FileName) + "Notepad Ultra Pro Max";
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|Rich Text Files(*.rtf)|*.rtf";
+            saveFileDialog.ShowDialog(this);
+
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Redo();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Copy(); 
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Paste();
+        }
+
+        private void selectAllToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.SelectAll();
+        }
+
+        private void dateTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Text=System.DateTime.Now.ToString();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MainRichTextBox.Font = fontDialog1.Font;
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MainRichTextBox.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void unodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainRichTextBox.Undo(); 
+        }
+    }
+}
